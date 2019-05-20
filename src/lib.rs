fcf4e85eaa88f2cacedd76e9947654b761221caa
Matt Hunzinger <matthunz2@gmail.com>
2019-05-19 23:32:53 -0400

    Create Cors with origins whitelist

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,26 @@
+use std::collections::HashSet;
+
+#[derive(Default)]
+pub struct Cors {
+    origins: Option<HashSet<&'static str>>,
+}
+
+impl Cors {
+    pub fn allow_origin(&mut self, origin: &'static str) -> &mut Self {
+        if let None = self.origins {
+            self.origins = Some(HashSet::new());
+        }
+        self.origins.as_mut().unwrap().insert(origin);
+        self
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+        let _cors = Cors::default().allow_origin("example.com");
     }
 }
