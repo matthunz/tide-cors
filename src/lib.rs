fb64d92dd70359a24df114c293f6abf96d630bdf
Matt Hunzinger <matthunz2@gmail.com>
2019-05-20 00:17:55 -0400

    Impl Middleware and verify origin

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,25 @@
+#![feature(async_await)]
+
+use futures::future::{BoxFuture, FutureExt};
 use std::collections::HashSet;
+use tide::http::StatusCode;
+use tide::http::header::{self, HeaderMap};
+use tide::{Context, Response};
+use tide::response::IntoResponse;
+use tide::middleware::{Middleware, Next};
+
+
+#[derive(Debug)]
+pub enum Error {
+    MissingOrigin,
+    OriginNotAllowed,
+}
+
+impl IntoResponse for Error {
+    fn into_response(self) -> Response {
+        ().with_status(StatusCode::FORBIDDEN).into_response()
+    }
+}
 
 #[derive(Default)]
 pub struct Cors {
@@ -13,7 +34,33 @@ impl Cors {
         self.origins.as_mut().unwrap().insert(origin);
         self
     }
+    fn validate_origin(&self, headers: &HeaderMap) -> Result<(), Error> {
+        if let Some(origins) = &self.origins {
+            let origin = headers
+                .get(header::ORIGIN)
+                .and_then(|hdr| hdr.to_str().ok())
+                .ok_or(Error::MissingOrigin)?;
+
+            if origins.contains(origin) {
+                Ok(())
+            } else {
+                Err(Error::OriginNotAllowed)
+            }
+        } else {
+            Ok(())
+        }
+    }
 }
+
+impl<Data: 'static + Send + Sync> Middleware<Data> for Cors {
+    fn handle<'a>(&'a self, cx: Context<Data>, next: Next<'a, Data>) -> BoxFuture<'a, Response> {
+        match self.validate_origin(cx.request().headers()) {
+            Ok(()) => next.run(cx),
+            Err(e) => FutureExt::boxed(async { e.into_response() }),
+        }
+    }
+}
+
 
 #[cfg(test)]
 mod tests {
